8c6157ab666fbf6f3f473e4ff4bd495c910187b6
Omprasad Rathod <20rathodomprasad@gmail.com>
2021-02-19 23:26:51 +0530

    Valid first name

--- MainClass.java
+++ MainClass.java
@@ -5,9 +5,17 @@ import java.util.regex.Pattern;
 
 public class MainClass {
 
-	
+	public static void validFirstName() {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter First Name : ");
+		String firstName = sc.nextLine();
+		if(Pattern.matches("^[A-Z][a-z]{2,}$", firstName))
+			System.out.println("Valid user name ");
+		else    
+			System.out.println("Invalid user name ");
+	}
 	public static void main(String[] args) {
-		System.out.println("Welcome");
+		validFirstName();
 
 	}
 
