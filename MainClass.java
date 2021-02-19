ba8f42a14a41f4d1b9378fd171ee9ce2420d6f90
Omprasad Rathod <20rathodomprasad@gmail.com>
2021-02-19 23:24:45 +0530

    Welcome

---
+++ MainClass.java
@@ -0,0 +1,14 @@
+package com.blbz;
+
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+public class MainClass {
+
+	
+	public static void main(String[] args) {
+		System.out.println("Welcome");
+
+	}
+
+}
